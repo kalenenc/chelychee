3dc64e78eb3a62203960718d64cb817c156fb51c
kalenenc <kalenenc@gmail.com>
2016-07-26 01:24:26 -0400

    added some transitions

--- JS/index.js
+++ JS/index.js
@@ -1,31 +1,32 @@
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('.nav-button').click(function(){
+// <script type="text/javascript">
+// 	$(document).ready(function(){
+// 		$('.nav-button').click(function(){
 
-			$('.nav-items').slideToggle();
-		});
+// 			$('.nav-items').slideToggle();
+// 		});
 
-		$(window).resize(function(){
+// 		$(window).resize(function(){
 
-			if ($(window).width() > 650) {
-				$('.nav-items')
-					.removeAttr('style')
-			}
+// 			if ($(window).width() > 650) {
+// 				$('.nav-items')
+// 					.removeAttr('style')
+// 			}
 			
-		});
+// 		});
 
-		$(".tan-people").hover(function() {
-			if ($(window).width() > 650) {
-				$(".main-pic-cover").style.display = "flex";
-			}
-			else {
-				$('.main-pic-cover').style.display = "flex";
-			};
+// 		$(".tan-people").hover(function() {
+// 			if ($(window).width() > 650) {
+// 				$(".main-pic-cover").style.display = "flex";
+// 			}
+// 			else {
+// 				$('.main-pic-cover').style.display = "flex";
+// 			};
 			
-		});
+// 		});
 		
-	});
+// 	});
 
 
 
-</script>
+// </script>
+
